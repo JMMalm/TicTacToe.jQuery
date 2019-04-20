a23e461bb9b8f5aea2e87ed01faccdcb51a1c5b9
Jordon Malm <jordon.malm@gmail.com>
2019-04-19 19:16:59 -0500

    Initial commit

---
+++ TicTacToe.jQuery/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿var xTurn = true;
+
+$(document).ready(function () {
+	console.log('jQuery has loaded.');
+
+	$('input.piece').on('click', function (event) {
+		if ($(this).hasClass('disabled')) {
+			return;
+		}
+
+		if (xTurn) {
+			$(this).addClass('x');
+			$(this).val('X');
+			$('#gameInfo > span').text('O\'s Turn!');
+			$('#gameInfo').addClass('o');
+		}
+		else {
+			$(this).addClass('o');
+			$(this).val('O');
+			$('#gameInfo > span').text('X\'s Turn!');
+			$('#gameInfo').removeClass('o');
+		}
+
+		$(this).addClass('disabled');
+		xTurn = !xTurn;
+	});
+});

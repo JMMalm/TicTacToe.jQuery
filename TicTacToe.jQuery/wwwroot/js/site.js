8e149cf17d52ddbe4cd02b33116ff6abd359281f
Jordon Malm <jordon.malm@gmail.com>
2019-04-20 10:38:13 -0500

    Implemented reset functionality and better organized the Javascript.

--- TicTacToe.jQuery/wwwroot/js/site.js
+++ TicTacToe.jQuery/wwwroot/js/site.js
@@ -3,25 +3,38 @@
 $(document).ready(function () {
 	console.log('jQuery has loaded.');
 
-	$('input.piece').on('click', function (event) {
+	$('input.piece').on('click', function () {
 		if ($(this).hasClass('disabled')) {
 			return;
 		}
 
-		if (xTurn) {
-			$(this).addClass('x');
-			$(this).val('X');
-			$('#gameInfo > span').text('O\'s Turn!');
-			$('#gameInfo').addClass('o');
-		}
-		else {
-			$(this).addClass('o');
-			$(this).val('O');
-			$('#gameInfo > span').text('X\'s Turn!');
-			$('#gameInfo').removeClass('o');
-		}
+		pieceSelected(this);
 
 		$(this).addClass('disabled');
 		xTurn = !xTurn;
 	});
 });
+
+function resetBoard() {
+	$('#gameGrid input.piece').each(function() {
+		$(this).val('');
+		$(this).removeClass('x o disabled');
+	});
+
+	xTurn = true;
+	$('#gameInfo > span').text('X\'s Turn!');
+	$('#gameInfo').removeClass('o');
+}
+
+function pieceSelected(boardPiece) {
+	if (xTurn) {
+		$(boardPiece).addClass('x').val('X');
+		$('#gameInfo > span').text('O\'s Turn!');
+		$('#gameInfo').addClass('o');
+	}
+	else {
+		$(boardPiece).addClass('o').val('O');
+		$('#gameInfo > span').text('X\'s Turn!');
+		$('#gameInfo').removeClass('o');
+	}
+}

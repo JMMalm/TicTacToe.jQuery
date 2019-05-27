96752aef5939227d1df298589d529e0f891a5ec3
Jordon Malm <jordon.malm@gmail.com>
2019-05-27 16:43:03 -0500

    Add logic for detecting game winner. Add new styling while removing others.

--- TicTacToe.jQuery/scripts/site.js
+++ TicTacToe.jQuery/scripts/site.js
@@ -1,4 +1,6 @@
-﻿var xTurn = true;
+﻿var isXTurn = true;
+var pieceArray;
+var placeHolderCharacter = '-';
 
 $(document).ready(function () {
 	console.log('jQuery has loaded.');
@@ -9,32 +11,79 @@ $(document).ready(function () {
 		}
 
 		pieceSelected(this);
-
 		$(this).addClass('disabled');
-		xTurn = !xTurn;
+
+		pieceArray = $('#gameGrid .piece').map(function() {
+			return $.trim($(this).text());
+		 }).get();
+
+		var gameWon = isGameWon(pieceArray);
+		if (gameWon) {
+			$('#gameGrid .piece').addClass('disabled');
+			var winnerMessage = 'The game has been won by ' + $(this).text() + '!'
+			$('#gameInfo > span').text(winnerMessage);
+			alert(winnerMessage);
+			$('#resetButton').addClass('btn-primary');
+		}
+		else {
+			updateGameInfo()
+		}
+		isXTurn = !isXTurn;
 	});
 });
 
 function resetBoard() {
+	$('#resetButton').removeClass('btn-primary');
 	$('#gameGrid .piece').each(function() {
-		$(this).text('-');
+		$(this).text(placeHolderCharacter);
 		$(this).removeClass('x o disabled');
 	});
 
-	xTurn = true;
+	isXTurn = true;
 	$('#gameInfo > span').text('X\'s Turn!');
 	$('#gameInfo').removeClass('o');
 }
 
 function pieceSelected(boardPiece) {
-	if (xTurn) {
+	if (isXTurn) {
 		$(boardPiece).addClass('x').text('X');
+	}
+	else {
+		$(boardPiece).addClass('o').text('O');
+	}
+}
+
+function updateGameInfo() {
+	if (isXTurn) {
 		$('#gameInfo > span').text('O\'s Turn!');
 		$('#gameInfo').addClass('o');
 	}
 	else {
-		$(boardPiece).addClass('o').text('O');
 		$('#gameInfo > span').text('X\'s Turn!');
 		$('#gameInfo').removeClass('o');
 	}
+}
+
+function isGameWon(gamePieces) {
+	return (
+		// Horizontal
+		isValidCombination(gamePieces[0], gamePieces[1], gamePieces[2])
+		|| isValidCombination(gamePieces[3], gamePieces[4], gamePieces[5])
+		|| isValidCombination(gamePieces[6], gamePieces[7], gamePieces[8])
+		// Vertical
+		|| isValidCombination(gamePieces[0], gamePieces[3], gamePieces[6])
+		|| isValidCombination(gamePieces[1], gamePieces[4], gamePieces[7])
+		|| isValidCombination(gamePieces[2], gamePieces[5], gamePieces[8])
+		// Diagonal
+		|| isValidCombination(gamePieces[0], gamePieces[4], gamePieces[8])
+		|| isValidCombination(gamePieces[2], gamePieces[4], gamePieces[6])
+	);
+}
+
+function isValidCombination(piece1, piece2, piece3) {
+	return (
+		// $.inArray will return -1 if the sought value is not found.
+		($.inArray(placeHolderCharacter, [piece1, piece2, piece3]) === -1)
+		&& (piece1 === piece2 && piece2 === piece3)
+	);
 }
